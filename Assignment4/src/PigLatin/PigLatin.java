173e16270d253e0faf1e34586895f0198e087feb
fergy <fergy@192.168.1.101>
2016-05-22 12:01:07 -0600

    time to commit

---
+++ Assignment4/src/PigLatin/PigLatin.java
@@ -0,0 +1,81 @@
+package PigLatin;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class PigLatin {
+
+	public static void main(String[] args) {
+		System.out.println("John Ferguson");
+		
+		System.out.println("Please enter a word (or press Q to quit): ");
+		
+		ArrayList<String> newSentence;
+		
+		String completeNewSentence = "";
+		
+		try (Scanner stringToTranslate = new Scanner(System.in)) {
+			
+			String translate = stringToTranslate.next();
+		
+				while (translate.toLowerCase().charAt(0) != 'q' || translate.length() != 1) {
+					
+					newSentence = new ArrayList<String>(0);
+					
+					if (translate.toLowerCase().charAt(0) != 'a' && translate.toLowerCase().charAt(0) != 'e' && translate.toLowerCase().charAt(0) != 'i' 
+								&& translate.toLowerCase().charAt(0) != 'o' && translate.toLowerCase().charAt(0) != 'u' )
+					{
+							while(translate.toLowerCase().charAt(0) != 'a' && translate.toLowerCase().charAt(0) != 'e' && translate.toLowerCase().charAt(0) != 'i' 
+									&& translate.toLowerCase().charAt(0) != 'o' && translate.toLowerCase().charAt(0) != 'u' )
+							{
+								char charToMove = translate.toLowerCase().charAt(0);
+								
+								translate = translate.substring(1);
+								
+								translate = translate + charToMove;
+								
+							}
+							
+							translate = translate + "ay";
+							
+							newSentence.add(translate);
+							
+					}
+					else if (translate.toLowerCase().charAt(0) == 'a' || translate.toLowerCase().charAt(0) == 'e' || translate.toLowerCase().charAt(0) == 'i' 
+							|| translate.toLowerCase().charAt(0) == 'o' || translate.toLowerCase().charAt(0) == 'u' )
+					{						
+						
+						translate = translate + "way";
+						
+						newSentence.add(translate);						
+						
+					}
+					
+					
+					for (int i = 0; i < newSentence.size(); i++)
+					{
+						if( i != newSentence.size()){
+							completeNewSentence += (newSentence.get(i) + " ");
+							newSentence.remove(i);
+						}
+						/*else{
+							completeNewSentence += (newSentence.get(i) + ".");
+						}*/
+						System.out.println(completeNewSentence);
+					}
+						
+						
+
+					System.out.println("Please enter a word (or press Q to quit): ");		
+							
+					translate = stringToTranslate.next();
+					
+					newSentence = new ArrayList<String>(0);
+				}
+				
+		
+		System.out.println("Thanks for playing!");
+		
+		}
+	}
+}
