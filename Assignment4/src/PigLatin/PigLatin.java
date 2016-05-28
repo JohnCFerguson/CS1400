8285d3027a908bcb790ccf270945b8c390f97a2e
fergy <fergy@192.168.1.101>
2016-05-28 11:02:51 -0600

    Commit the new!

--- Assignment4/src/PigLatin/PigLatin.java
+++ Assignment4/src/PigLatin/PigLatin.java
@@ -1,80 +1,91 @@
 package PigLatin;
 
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class PigLatin {
+	
+	private static String translatedString = "";
+	
+	private static String translate = " ";
 
 	public static void main(String[] args) {
+		
 		System.out.println("John Ferguson");
 		
-		System.out.println("Please enter a word (or press Q to quit): ");
-		
-		ArrayList<String> newSentence = new ArrayList<String>(0);
-		
-		String completeNewSentence = "";
-		
-		try (Scanner stringToTranslate = new Scanner(System.in)) {
-			
-			String translate = stringToTranslate.next();
-		
-				while (translate.toLowerCase().charAt(0) != 'q' || translate.length() != 1) { 
-					
-					if (translate.toLowerCase().charAt(0) != 'a' && translate.toLowerCase().charAt(0) != 'e' && translate.toLowerCase().charAt(0) != 'i' 
-								&& translate.toLowerCase().charAt(0) != 'o' && translate.toLowerCase().charAt(0) != 'u' )
-					{
-							while(translate.toLowerCase().charAt(0) != 'a' && translate.toLowerCase().charAt(0) != 'e' && translate.toLowerCase().charAt(0) != 'i' 
-									&& translate.toLowerCase().charAt(0) != 'o' && translate.toLowerCase().charAt(0) != 'u' )
-							{
-								char charToMove = translate.toLowerCase().charAt(0);
-								
-								translate = translate.substring(1);
-								
-								translate = translate + charToMove;
-								
-							}
-							
-							translate = translate + "ay";
-							
-							newSentence.add(translate);
-							
-					}
-					else if (translate.toLowerCase().charAt(0) == 'a' || translate.toLowerCase().charAt(0) == 'e' || translate.toLowerCase().charAt(0) == 'i' 
-							|| translate.toLowerCase().charAt(0) == 'o' || translate.toLowerCase().charAt(0) == 'u' )
-					{						
-						
-						translate = translate + "way";
-						
-						newSentence.add(translate);						
-						
-					}
-					
-					
-					for (int i = 0; i < newSentence.size(); i++)
-					{
-						if( i != newSentence.size()){
-							completeNewSentence += (newSentence.get(i) + " ");
-							newSentence.remove(i);
-						}
+		while (translate.toLowerCase() != "q" || translate.length() != 1) {
+			try {
 
-						//System.out.println(completeNewSentence);
-					}
-						
-						
-					System.out.println(completeNewSentence);
+				@SuppressWarnings("resource")
+				Scanner stringToTranslate = new Scanner(System.in);
+				
+				System.out.println("Please enter a word (or press Q to quit): ");
+				
+				translate = stringToTranslate.nextLine();
+				
+				ArrayList<String>newSentence = new ArrayList<String>(Arrays.asList(translate.split("\\s+")));
+				
+				translateNewSentence(newSentence);
+				
+				System.out.println(translatedString);
+				
+				translatedString = "";
+			}
+
 					
-					System.out.println("Please enter a word (or press Q to quit): ");		
-							
-					translate = stringToTranslate.next();
+			catch(Exception e) {
+				
+				System.out.println("You did not input anything");
 					
-					newSentence.clear();
-					
-					//completeNewSentence = "";
-				}
+			}
 				
 		
-		System.out.println("Thanks for playing!");
-		
+				
+			
 		}
+		System.out.println("Thanks for playing!");
+	}	
+	
+	
+	public static String translateNewSentence(ArrayList<String> stringToTranslate) {		
+		//System.out.print("what is even going on");
+
+		
+		//System.out.println(newSentence);
+			
+		for (int i = 0; i < stringToTranslate.size(); i++){
+				
+			if (stringToTranslate.get(i).toLowerCase().charAt(0) != 'a' && stringToTranslate.get(i).toLowerCase().charAt(0) != 'e' && stringToTranslate.get(i).toLowerCase().charAt(0) != 'i' 
+						&& stringToTranslate.get(i).toLowerCase().charAt(0) != 'o' && stringToTranslate.get(i).toLowerCase().charAt(0) != 'u' ) {
+				
+					while(stringToTranslate.get(i).toLowerCase().charAt(0) != 'a' && stringToTranslate.get(i).toLowerCase().charAt(0) != 'e' && stringToTranslate.get(i).toLowerCase().charAt(0) != 'i' 
+							&& stringToTranslate.get(i).toLowerCase().charAt(0) != 'o' && stringToTranslate.get(i).toLowerCase().charAt(0) != 'u' ){
+						
+						char charToMove = stringToTranslate.get(i).toLowerCase().charAt(0);
+						
+						stringToTranslate.set(i, stringToTranslate.get(i).substring(1));
+						
+						stringToTranslate.set(i,stringToTranslate.get(i) + charToMove);
+						
+					}
+					
+					stringToTranslate.set(i, stringToTranslate.get(i) + "ay ");
+					
+					translatedString += stringToTranslate.get(i).toString();
+					
+			}
+			else if (stringToTranslate.get(i).toLowerCase().charAt(0) == 'a' || stringToTranslate.get(i).toLowerCase().charAt(0) == 'e' || stringToTranslate.get(i).toLowerCase().charAt(0) == 'i' 
+					|| stringToTranslate.get(i).toLowerCase().charAt(0) == 'o' || stringToTranslate.get(i).toLowerCase().charAt(0) == 'u' ){						
+				
+				stringToTranslate.set(i, stringToTranslate.get(i) + "way ");
+				
+				translatedString += stringToTranslate.get(i).toString();						
+				
+			}
+		}
+
+		return translatedString;		
 	}
 }
+
